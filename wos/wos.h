1bc544383d7edf9841f1556b8b04f547346df791
Pranav Jain <pranav.jain70@gmail.com>
2025-12-02 17:55:57 -0800

    changed function definitions

--- wos/wos.h
+++ wos/wos.h
@@ -4,10 +4,6 @@
 #include <array>
 #include <random>
 
-double distance_to_boundary(const std::array<double,3>& x);
-
-double boundary_value(const std::array<double,3>& x);
-
 std::array<double,3> random_point_on_sphere(
     const std::array<double,3>& x0, 
     double r, 
@@ -16,6 +12,8 @@ std::array<double,3> random_point_on_sphere(
 double walk_on_spheres(
     const std::array<double,3>& x0,
     double eps,
-    std::mt19937& rng);
+    std::mt19937& rng,
+    std::function<double(const std::array<double,3>&)> sdf,
+    std::function<double(const std::array<double,3>&)> boundary_value);
 
 #endif
